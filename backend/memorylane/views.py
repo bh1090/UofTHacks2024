b1dc4b584a3a1d8421feea1a955011156522d302
Swithord <yorkhayng@gmail.com>
2024-01-27 15:55:53 -0500

    retrieve address from coordinates

--- backend/memorylane/views.py
+++ backend/memorylane/views.py
@@ -6,14 +6,9 @@ from django.http import JsonResponse
 from streetview import search_panoramas
 from streetview import get_streetview
 import os
+import requests
+import regex as re
 
-def conform_coordinates(latitude, longitude):
-    try:
-        # Convert the string to a float with 4 decimal points
-        return '{:.4f}'.format(float(latitude)), '{:.4f}'.format(float(longitude))
-    except ValueError:
-        # Handle the case where the input string is not a valid number
-        assert False
 
 def get_location_view(request):
     # Retrieve latitude and longitude parameters from the request
@@ -21,18 +16,26 @@ def get_location_view(request):
     longitude = request.GET.get('lon')
 
     # Generate image URL based on latitude and longitude
-    image_urls = generate_image_urls(conform_coordinates(latitude, longitude))
+    image_urls = get_image_urls(conform_coordinates(latitude, longitude))
+
+    # Retrieve address corresponding to the coordinates from Nominatim
+    address = get_address(latitude, longitude)
+
+    # Generate summary of the location with Cohere
+    summary = get_summary(address)
 
     # Create JSON response containing the image URL
     response_data = {
-        'image_urls': image_urls
+        'image_urls': image_urls,
+        'address': address,
+        'summary': summary
     }
 
     return JsonResponse(response_data)
 
 
-def generate_image_urls(coordinate):
-    panos = search_panoramas(lat=coordinate[0], lon=coordinate[1])
+def get_image_urls(coordinate: tuple[str, str]) -> list[str]:
+    panos = search_panoramas(lat=float(coordinate[0]), lon=float(coordinate[1]))
     pano_ids = {x.pano_id: x.date for x in panos if x.date}
     results = []
     dir = f"images/{coordinate[0]},{coordinate[1]}/"
@@ -46,3 +49,26 @@ def generate_image_urls(coordinate):
         results.append(name)
     return results
 
+
+def conform_coordinates(latitude: str, longitude: str) -> tuple[str, str]:
+    try:
+        # Convert the string to a float with 4 decimal points
+        return '{:.4f}'.format(float(latitude)), '{:.4f}'.format(float(longitude))
+    except ValueError:
+        # Handle the case where the input string is not a valid number
+        assert False
+
+
+def get_address(latitude: str, longitude: str) -> str:
+    response = requests.get('https://nominatim.openstreetmap.org/reverse', params={
+        'format': 'json',
+        'lat': latitude,
+        'lon': longitude
+    }).json()
+    address = ','.join(response['display_name'].split(',')[0:5])
+    # Remove special characters
+    return re.sub(r'[^\x00-\x7F]+', '', address)
+
+
+def get_summary(address: str) -> str:
+    return ''
